0e3d4844268d2cfa53485c50a35a30b3a099ae23
Maven <maven@concord.org>
2005-09-09 14:01:58 +0000

    Adding licenses

--- sensor-native/src/java/org/concord/sensor/nativelib/NativePseudoSensorDevice.java
+++ sensor-native/src/java/org/concord/sensor/nativelib/NativePseudoSensorDevice.java
@@ -1,7 +1,6 @@
-
 /*
  *  Copyright (C) 2004  The Concord Consortium, Inc.,
- *  10 Concord Crossing, Concord, MA 01741
+ *  10 Concord Crossing, Concord, MA 01742
  *
  *  Web Site: http://www.concord.org
  *  Email: info@concord.org
@@ -20,12 +19,12 @@
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- */
+ * END LICENSE */
 
 /*
  * Last modification information:
- * $Revision: 1.4 $
- * $Date: 2005-06-15 14:10:37 $
+ * $Revision: 1.5 $
+ * $Date: 2005-09-09 14:01:58 $
  * $Author: maven $
  *
  * Licence Information

94bb2b373b9d4a3550da9b099299ec539fe5fa80
Scott Cytacki <scytacki@concord.org>
2007-02-28 22:21:55 +0000

    added folder in the Sensor project to store deployment scripts for rxtx into maven2 repository

---
+++ sensor/rxtx-maven2/deploy.sh
@@ -0,0 +1,18 @@
+cd $1
+source properties
+
+DEPLOY_URL=scpexe://source.concord.org/web/source.concord.org/html/software/maven2/internal/
+DEPLOY_ID=cc-dist-repo-internal
+CLASSIFIER=${PLATFORM}
+NAR_FILE=../../target/${ARTIFACT_ID}-${PLATFORM}.nar
+
+# need to build the nar file of the linux native libraries
+jar cf ${NAR_FILE} ${NATIVE_FILES}
+
+mvn deploy:deploy-file -Dfile=${NAR_FILE} \
+    -DartifactId=${ARTIFACT_ID} -DgroupId=${GROUP_ID} \
+    -Dclassifier=${CLASSIFIER} \
+    -Dversion=${VERSION} -Dpackaging=nar \
+    -Durl=${DEPLOY_URL} -DrepositoryId=${DEPLOY_ID}
+
+cd -
---
+++ sensor/rxtx-maven2/readme.txt
@@ -0,0 +1,6 @@
+These maven files are here for historical reasons. 
+The native libraries for rxtx have been kept in this project.
+They should be moved somewhere else and then these maven
+files should be moved too.  If the nar packaging and dependencies
+are correctly handled by eclipse then these files will 
+not need to be checked out at all.

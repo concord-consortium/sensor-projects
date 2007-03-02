e50697b8865bfc50429828d92cd979e4d6e7534c
Scott Cytacki <scytacki@concord.org>
2007-03-02 19:56:25 +0000

    script and config for deploying nar of windows dll

---
+++ ftdi-serial-wrapper/maven2-nar/deploy.sh
@@ -0,0 +1,18 @@
+#this should be turned into a plugin, or we should try to use 
+#already existing nar plugin
+
+source properties
+
+DEPLOY_URL=scpexe://source.concord.org/web/source.concord.org/html/software/maven2/internal_snapshot
+DEPLOY_ID=cc-dist-repo-internal
+CLASSIFIER=${PLATFORM}
+NAR_FILE=../target/${ARTIFACT_ID}-${PLATFORM}.nar
+
+# need to build the nar file of the linux native libraries
+jar cf ${NAR_FILE} ${NATIVE_FILES}
+
+mvn deploy:deploy-file -Dfile=${NAR_FILE} \
+    -DartifactId=${ARTIFACT_ID} -DgroupId=${GROUP_ID} \
+    -Dclassifier=${CLASSIFIER} \
+    -Dversion=${VERSION} -Dpackaging=nar \
+    -Durl=${DEPLOY_URL} -DrepositoryId=${DEPLOY_ID}

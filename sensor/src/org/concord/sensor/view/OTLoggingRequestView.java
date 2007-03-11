7e56eb99f1ff691bba6e2479675d9eefe9462739
Scott Cytacki <scytacki@concord.org>
2007-03-11 23:38:52 +0000

    Major non backward compatible change.  Changed view service setup, now views access the OTViewFactory, OTrunk, OTFrameManager, and OTUserListService though an OTViewServiceProvider interface.

--- sensor/src/org/concord/sensor/view/OTLoggingRequestView.java
+++ sensor/src/org/concord/sensor/view/OTLoggingRequestView.java
@@ -23,8 +23,8 @@
 
 /*
  * Last modification information:
- * $Revision: 1.3 $
- * $Date: 2007-03-09 17:51:59 $
+ * $Revision: 1.4 $
+ * $Date: 2007-03-11 23:38:46 $
  * $Author: scytacki $
  *
  * Licence Information
@@ -36,17 +36,16 @@ import javax.swing.JComponent;
 
 import org.concord.framework.otrunk.OTObject;
 import org.concord.framework.otrunk.OTObjectService;
+import org.concord.framework.otrunk.view.AbstractOTView;
 import org.concord.framework.otrunk.view.OTActionView;
 import org.concord.framework.otrunk.view.OTJComponentView;
 import org.concord.framework.otrunk.view.OTViewFactory;
-import org.concord.framework.otrunk.view.OTViewFactoryAware;
 import org.concord.sensor.state.OTLoggingRequest;
 import org.concord.sensor.state.OTSetupLogger;
 
-public class OTLoggingRequestView
-    implements OTJComponentView, OTViewFactoryAware
+public class OTLoggingRequestView extends AbstractOTView
+    implements OTJComponentView
 {
-    protected OTViewFactory viewFactory;
     protected OTLoggingRequest request;
     
     /**
@@ -58,6 +57,9 @@ public class OTLoggingRequestView
      */
     public JComponent getComponent(OTObject otObject, boolean editable)
     {
+    	OTViewFactory viewFactory = 
+    		   (OTViewFactory)getViewService(OTViewFactory.class);
+
         request = (OTLoggingRequest)otObject;
 
         try {
@@ -87,8 +89,4 @@ public class OTLoggingRequestView
         // TODO Auto-generated method stub
         
     }
-
-	public void setViewFactory(OTViewFactory factory) {
-		viewFactory = factory;		
-	}
 }

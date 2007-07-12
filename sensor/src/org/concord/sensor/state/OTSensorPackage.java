82343be74817f0483b3fbf47dfb63df094184411
Scott Cytacki <scytacki@concord.org>
2007-07-12 18:07:55 +0000

    This is another broad sweeping commit.  It contains a few changes mixed together. A new method was added to the OTPackage interface so users of the package can get a list of all the OTClasses in the package.  This is useful for generating schemas for the package. The way to access the OTControllerService was changed.  It used to be through the OTObjectService, now it should be accessed through the viewServiceProvider. A new feature was added to the OTControllerService so that individual controllers can indicate if their realObjects can be shared with sibling controller services.  These shared realObjects are currently stored in a controllerService managed by the  OTViewContainerPanel.  The viewContanerPanel disposes the shared controller each time its current object changes.

--- sensor/src/org/concord/sensor/state/OTSensorPackage.java
+++ sensor/src/org/concord/sensor/state/OTSensorPackage.java
@@ -3,6 +3,9 @@
  */
 package org.concord.sensor.state;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.concord.framework.otrunk.OTControllerRegistry;
 import org.concord.framework.otrunk.OTPackage;
 import org.concord.framework.otrunk.OTrunk;
@@ -24,6 +27,22 @@ public class OTSensorPackage
 			(OTControllerRegistry) otrunk.getService(OTControllerRegistry.class);
 		
 		registry.registerControllerClass(OTSensorDataProxyController.class);
+	}
+
+	public List getOTClasses() 
+	{
+		ArrayList list = new ArrayList();
+
+		list.add(OTDeviceConfig.class);
+		list.add(OTExperimentRequest.class);
+		list.add(OTInterfaceManager.class);
+		list.add(OTLoggingRequest.class);
+		list.add(OTSensorDataProxy.class);
+		list.add(OTSensorRequest.class);
+		list.add(OTSetupLogger.class);
+		list.add(OTZeroSensor.class);
+		
+		return list;
 	}
 
 }

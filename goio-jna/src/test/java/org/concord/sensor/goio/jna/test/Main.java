7c1b27f12ac3bdeda2f5408867aab7110f1e4700
Johan van der Hoeven <lonoami@gmail.com>
2010-06-07 20:41:33 +0000

    Tests init function in the native Go IO library

--- goio-jna/src/test/java/org/concord/sensor/goio/jna/test/Main.java
+++ goio-jna/src/test/java/org/concord/sensor/goio/jna/test/Main.java
@@ -1,5 +1,34 @@
 package org.concord.sensor.goio.jna.test;
 
+import java.io.IOException;
+
+import org.concord.sensor.goio.jna.*;
+
+
+//Test GoIO
 public class Main {
+	
+	
+	private static GoIOInterface goIOInterface;
+
+	public static void main(String[] args) throws IOException {
+		
+		boolean sweet = false;
+		goIOInterface = new GoIOInterface();
+		
+		System.out.println("start main");
+		
+		sweet = goIOInterface.init();
+		
+		if(!sweet)
+		{
+			System.out.println("goIOInterface.init() failed --bye");
+			return;
+		}
+		
+		
+		System.out.println("end  main");
+	};//end main
 
 }
+

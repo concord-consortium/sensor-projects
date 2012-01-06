1128faa15f6efa51d2b3d9bdf0b158ba1f54dc27
Scott Cytacki <scytacki@concord.org>
2012-01-06 20:40:10 +0000

    buffer printing support
    additionally document how to process all datasheets

--- pasco-jna/src/main/java/org/concord/sensor/pasco/datasheet/PrintDataSheetFile.java
+++ pasco-jna/src/main/java/org/concord/sensor/pasco/datasheet/PrintDataSheetFile.java
@@ -3,6 +3,13 @@ package org.concord.sensor.pasco.datasheet;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+/*
+ * you can run this over all the files from Pasco something this:
+ * > for file in /Applications/DataStudio.app/Contents/Resources/*.sds; do java -Dfile.encoding=MacRoman -classpath /Users/scytacki/Documents/EclipseWorkspace-3.5/pasco-jna/target/classes:/Users/scytacki/.m2/repository/org/concord/external/jna/jna-jws/3.0.9/jna-jws-3.0.9.jar org.concord.sensor.pasco.datasheet.PrintDataSheetFile "$file"; done > datasheets.txt 
+ * @author scytacki
+ *
+ */
+
 public class PrintDataSheetFile {
 
 	/**
--- pasco-jna/src/main/java/org/concord/sensor/pasco/datasheet/Printer.java
+++ pasco-jna/src/main/java/org/concord/sensor/pasco/datasheet/Printer.java
@@ -28,4 +28,62 @@ public class Printer {
 		System.out.print(result);
 	}
 	
+	public void printBuffer(byte[] pvBuf, int lLen)
+	{
+	    byte[] pbBuf = pvBuf;
+		int lBytes = 1;
+		
+	    // for each 16 byte row
+	    for (int nRow = 0; nRow <= (lLen-1)/16; nRow++)
+	    {
+	        String pszStr = "";
+			
+	        // fill in the hex view of the data
+	        int nCol;
+	        for (nCol = 0; nCol < 16; nCol+=lBytes)
+	        {
+	            switch (lBytes)
+	            {
+					case 1: // show as bytes
+					default:
+						if (nRow*16+nCol < lLen)
+							pszStr += String.format("%02x ", pbBuf[nRow*16+nCol]);
+						else
+							pszStr += String.format("   ");
+						break;
+						
+					case 2: // show as shorts
+						if (nRow*16+nCol < lLen)
+							pszStr += String.format ("%04x ", pbBuf[nRow*16+nCol]);
+						else
+							pszStr += String.format("     ");
+						break;
+						
+					case 4: // show as longs
+						if (nRow*16+nCol < lLen)
+							pszStr += String.format ("%08x ", pbBuf[nRow*16+nCol]);
+						else
+							pszStr += String.format ("         ");
+						break;
+	            }
+	        }
+			
+	        // fill in the char view of the data
+	        for (nCol = 0; nCol < 16; nCol++)
+	        {
+	            if (nRow*16+nCol < lLen)
+	            {
+	                byte ch = pbBuf[nRow*16+nCol];
+	                if (ch < 0x20 || ch >= 0x7f)
+	                    pszStr += ".";
+	                else
+	                    pszStr += String.format("%c", ch);
+	            }
+	            else
+	                pszStr += " ";
+	        }
+			
+	        puts(String.format( " %05x: %s", nRow*0x10, pszStr));
+	    }
+	}
 }
